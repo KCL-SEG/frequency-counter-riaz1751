df88eafdeb4c5ba4df589c799920ffd7c2619ddd
k21062319 <muhammad.r.rahman@kcl.ac.uk>
2022-10-03 21:28:56 +0100

    Create dictionary from item list.

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    return frequencies
+    for i in items:
+        if str(i) not in frequencies.keys():
+            frequencies[str(i)] = 1
+        else:
+            value = frequencies.get(str(i))
+            print(value)
+            frequencies[str(i)] = value + 1
+    return(frequencies)
